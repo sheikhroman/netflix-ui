d6edbe25135a0d679a35a138f761c2890b94a562
Roman <sheikhroman042@gmail.com>
2023-02-12 16:31:27 +0600

    navbar done

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
+import backgroundImage from '../assets/home.jpg';
+import MovieLogo from '../assets/homeTitle.webp';
 import Navbar from '../components/Navbar';
+import { FaPlay } from 'react-icons/fa';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
+import styled from 'styled-components';
 
 export default function Netflix() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -7,7 +12,21 @@ export default function Netflix() {
     setIsScrolled(window.pageXOffset === 0 ? false : true);
     return () => (window.onscroll = null);
   }
-  return <div className="">
+  return (
+    <Container> 
     <Navbar isScrolled={isScrolled} />
-  </div>
+    <div className="hero">
+      <img src={backgroundImage} alt="background" className='background-image' />
+      <div className="container">
+        <div className="logo">
+          <img src={MovieLogo} alt="Movie Logo" />
+        </div>
+        <div className="buttons flex">
+          <button className='flex j-center a-center'><FaPlay />Play</button>
+          <button className='flex j-center a-center'><AiOutlineInfoCircle />More Info</button>
+        </div>
+      </div>
+    </div>
+    </Container>
+  )
 }
